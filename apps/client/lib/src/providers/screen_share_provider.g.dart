9424020cf72b32713195945daf1e4d1c880c4141
NC1107 <nickpconn@gmail.com>
2026-05-05 10:46:25 -0400

    refactor(client): migrate screen_share_provider to @Riverpod codegen

---
+++ apps/client/lib/src/providers/screen_share_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'screen_share_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$screenShareHash() => r'2aba7f83789a88d37543ddddf5a55ee439a448f9';
+
+/// See also [ScreenShare].
+@ProviderFor(ScreenShare)
+final screenShareProvider =
+    NotifierProvider<ScreenShare, ScreenShareState>.internal(
+      ScreenShare.new,
+      name: r'screenShareProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$screenShareHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$ScreenShare = Notifier<ScreenShareState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
